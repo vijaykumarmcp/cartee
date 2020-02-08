9a2c6dd1c9a064d4eed530531965c18c8c2225d5
vijaykumarmcp <43601069+vijaykumarmcp@users.noreply.github.com>
2020-02-09 01:01:47 +0530

    added checkout,tracker fetuares

--- cartee/mac/views.py
+++ cartee/mac/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
-from .models import Product,Contact
+from .models import Product,Contact,Orders,OrderUpdate
 from math import ceil
 
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 # Create your views here.
 
 def index(request):
@@ -34,7 +39,24 @@ def contact(request):
     return render(request,'mac/contact.html')
 
 def tracker(request):
-    return render(request,'mac/tracker.html')
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
+    return render(request, 'mac/tracker.html')
 
 def search(request):
     return HttpResponse('you are at search')
@@ -44,7 +66,23 @@ def prodView(request,myid):
     return render(request,'mac/prodView.html',{'product':product[0]})
 
 def checkout(request):
-    return HttpResponse('you are at checkout')
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'mac/checkout.html', {'thank':thank, 'id': id})
+    return render(request, 'mac/checkout.html')
+
 
 def products(request):
         return HttpResponse(models.Product.all)
